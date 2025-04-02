7b32262659ac75bef86ca814760cad080bd0c245
venkatesh-08775 <125874744+venkatesh-08775@users.noreply.github.com>
2025-04-02 06:52:16 +0530

    344

---
+++ two-pointer/src/reverse_string/Solution.java
@@ -0,0 +1,28 @@
+package reverse_string;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        char[] s = {'H','a','n','n','a','h'};
+        char[] result = reverseString(s);
+        System.out.println(Arrays.toString(result));
+    }
+
+    private static char[] reverseString(char[] s){
+        int start = 0;
+        int end = s.length-1;
+
+
+        while (start < end){
+            char temp  = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        return  s;
+    }
+}
